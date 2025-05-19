5deab32bb7c56691c4351fde11010d262cf00465
Katharina <katharinahoelzlvienna@gmail.com>
2025-05-19 11:52:39 +0200

    New Files

---
+++ run_keyword_test.py
@@ -0,0 +1,31 @@
+import csv
+from selenium import webdriver
+from keyword_library import KeywordLibrary
+
+def run_test(csv_file):
+    driver = webdriver.Chrome()
+    keywords = KeywordLibrary(driver)
+
+    #Open and/or read the csv file
+    with open(csv_file, newline="") as file:
+        reader = csv.DictReader(file)
+        #Create a loop through each row in the csv
+        for row in reader:
+            keyword = row["Keyword"]
+            locator_type = row["LocatorType"]
+            locator_value = row["LocatorValue"]
+            data = row["Data"]
+            #Check existence of the method in the KeywordLibrary
+            if hasattr(keywords,keyword): #hasattr chekcs if the class contains a method like the keyword
+                method = getattr #getatt gets the actual function so thwt w can call it later
+                #Conditional method calling
+                if locator_type and locator_value and data:
+                    method(locator_type,locator_value,data)
+                elif locator_type and locator_value:
+                    method(locator_type,locator_value)
+                elif data:
+                    method(data)
+                else:
+                    method()
+            else:
+                print(f" Keyword is unknown: {keyword}")
